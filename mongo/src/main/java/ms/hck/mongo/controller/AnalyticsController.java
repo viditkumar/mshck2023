5540c380b8d9c5dee65080ce2f960a67a3c23104
unknown <viditkumar.vk@gmail.com>
2023-03-25 22:11:24 +0530

    Initial Commit

---
+++ mongo/src/main/java/ms/hck/mongo/controller/AnalyticsController.java
@@ -0,0 +1,9 @@
+package ms.hck.mongo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnalyticsController {
+    
+}
